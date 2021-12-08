16ed8cbcd91fcb8ad88a265d78a557769d420849
Juan David Tovar Montoya <davidtovarmontoya@gmail.com>
2021-12-07 19:08:31 -0500

    initial commit

---
+++ api/src/models/Videogame.js
@@ -0,0 +1,12 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le inyectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('videogame', {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
