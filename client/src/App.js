05a2995f153b655e1030a765d90424f10ea61945
Juan David Tovar Montoya <davidtovarmontoya@gmail.com>
2021-12-29 17:47:09 -0500

    app.js finished

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Landing from "./components/Landing/Landing";
+import Home from "./components/Home/Home";
+import AddVideogame  from './components/AddVideogame/AddVideogame.jsx'
+import GameDetail  from './components/GameDetail/GameDetail.jsx'
+
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Videogames</h1>
+      <Routes>
+        <Route exact path="/" element={<Landing />} />
+        <Route path="/home" element={<Home />} />
+        <Route path= '/gamedetail/:id' element = {<GameDetail/>}/>
+        <Route path= '/addgame' element = {<AddVideogame/>}/>
+      </Routes>
     </div>
   );
 }
