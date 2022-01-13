c2d0d56002ebcf15f683b836e3cde5a74468a794
Juan David Tovar Montoya <davidtovarmontoya@gmail.com>
2022-01-13 17:31:26 -0500

    deploying 2.0

--- client/src/index.js
+++ client/src/index.js
@@ -4,20 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { BrowserRouter } from 'react-router-dom';
+import {  HashRouter } from 'react-router-dom';
+
 import { Provider } from "react-redux";
 import store from './store/index.js';
 
 ReactDOM.render(
-  <React.StrictMode>
+
     <Provider store={store}>
-      <BrowserRouter>
+      <HashRouter >
         <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
+      </HashRouter>
+    </Provider>,
+
   document.getElementById("root")
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
